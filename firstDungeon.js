e15a9ae7fc96c06fc55d1abc680b06b48a78931d
nickcl <kidscline02@gmail.com>
2017-05-20 19:51:50 -0700

    created firstdungeon.js

---
+++ firstDungeon.js
@@ -0,0 +1,17 @@
+(function(){
+    
+    class firstDungeon{
+        //type 1 is ground button
+        //type 2 is floor
+        //type 3 is wall
+        //type 4 is door
+        //type 5 is wall button
+        //type 6 is boss
+
+
+
+    }
+
+
+
+}())
