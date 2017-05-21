ef3840896826aa2fb81903a90c52c1b589abb39d
nickcl <kidscline02@gmail.com>
2017-05-20 17:57:28 -0700

    added eventemitter

--- dungeon.js
+++ dungeon.js
@@ -1,29 +1,51 @@
 
 (function(){
     
+    const EventEmitter = require('events');
     class Dungeon{
+        
         //objects is just all the objects
-        //switchvals is an array of functions, indexes identify what they are and functions show which values to change
+        //switchvals is an array of arrays []
         //bosses is a bosses' stats, [its in an array because there can be multiple bosses[switchValsded,boolDed]
-        constructor(objects,switchVals,bosses){
-            this.bosses = bosses;
+        constructor(objects,switchVals,game){
             this.objects = objects;
             this.switchVals = switchVals;
-        }
-        changeSwitchVal(switchindex){
-            switchVals[switchindex]!=switchVals[switchindex];
-        }
-        changeBossStatus(boss,switchVals){
-            if(switchVals==boss[0]){
-                boss[1]=true;
+            this.game=game;
+            this.events=new EventEmitter();
+            var events=this.events
+            for(var player in game){
+                game[player].socket.messages.on("keyPress",function(data){
+                    var key=data.readInt8(0)
+                    if(key==32 || key==101){
+                        
+                        for(var switchpos in switchVals){
+                            var xval=switchVals[switchpos].x;
+                            var yval=switchVals[switchpos].y;
+                            var playerx=game[player].x;
+                            var playery=game[player].y;
+                            var distance=sqrt(pow((xval-playerx),2)-pow((yval-playery),2));
+                            if(distance<3){
+                                events.emit('switchhit');
+                                
+                            }
+                        
+
+                        }
+                        
+                    }
+
+
+                });
             }
         }
 
+        
+        }
 
 
 
 
-    }
+
     module.exports=Dungeon;
 
 
